52048c77909ebcb8a4671b7e4a5b1bd3ed49ff26
havanviet6789 <havan022006@gmail.com>
2026-04-23 15:41:05 +0700

    Update caesar.cpp

--- src/caesar.cpp
+++ src/caesar.cpp
@@ -25,15 +25,13 @@ char shift_char(char c, int shift) {
 string caesar_encrypt(const string &plaintext, int shift) {
     string ciphertext;
     for (char c : plaintext) {
-        // TODO(student): Q1 + Q2
-        ciphertext += shift_char(c, shift);
+        ciphertext += shift_char(c, shift); // đã xử lý dịch ký tự
     }
     return ciphertext;
 }
 
 string caesar_decrypt(const string &ciphertext, int shift) {
-    // TODO(student): Q3
-    return caesar_encrypt(ciphertext, -shift);
+    return caesar_encrypt(ciphertext, -shift); // giải mã bằng dịch ngược
 }
 
 int main() {
